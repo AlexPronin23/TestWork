107e6fbdf923a8cbac6fc96f6b345c5bad58c209
AlexPronin23 <apronin2005@gmail.com>
2026-07-19 21:14:26 +0700

    Second part: Added fetch query(got users with limit)

--- testWork/src/components/Table.jsx
+++ testWork/src/components/Table.jsx
@@ -1,79 +1,65 @@
+import { useEffect, useState } from "react";
+import TableItem from "./TableItem";
+
 const Table = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  async function fetchUsers() {
+    try {
+      const response = await fetch(
+        "https://dummyjson.com/users?limit=5&select=firstName,lastName,maidenName,age,gender,email,phone,address",
+      );
+
+      if (!response.ok) {
+        let message = `Error happened :  ${response.status} - ${response.statusText}`;
+        throw new Error(message);
+      }
+
+      const data = await response.json();
+
+      setUsers(data.users);
+      setLoading(false);
+    } catch (error) {
+      console.log(error.message);
+      setError(error.message);
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="table__wrapper">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Имя</th>
-            <th>Фамилия</th>
-            <th>Отчество</th>
-            <th>Возраст</th>
-            <th>Пол</th>
-            <th>Номер телефона</th>
-            <th>Email</th>
-            <th>Страна</th>
-            <th>Город</th>
-          </tr>
-        </thead>
+      {loading ? (
+        <>
+          <h1 className="table__loading">Загрузка...</h1>
+        </>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Имя</th>
+              <th>Фамилия</th>
+              <th>Отчество</th>
+              <th>Возраст</th>
+              <th>Пол</th>
+              <th>Номер телефона</th>
+              <th>Email</th>
+              <th>Страна</th>
+              <th>Город</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          <tr>
-            <td>Gloria</td>
-            <td>Reeves</td>
-            <td>67439</td>
-            <td>10/18/1985</td>
-            <td>4</td>
-            <td>0.1</td>
-            <td>$50</td>
-            <td>Usa</td>
-            <td>UTA</td>
-          </tr>
-          <tr>
-            <td>Gloria</td>
-            <td>Reeves</td>
-            <td>67439</td>
-            <td>10/18/1985</td>
-            <td>4</td>
-            <td>0.1</td>
-            <td>$50</td>
-            <td>Usa</td>
-            <td>UTA</td>
-          </tr>
-          <tr>
-            <td>Gloria</td>
-            <td>Reeves</td>
-            <td>67439</td>
-            <td>10/18/1985</td>
-            <td>4</td>
-            <td>0.1</td>
-            <td>$50</td>
-            <td>Usa</td>
-            <td>UTA</td>
-          </tr>
-          <tr>
-            <td>Gloria</td>
-            <td>Reeves</td>
-            <td>67439</td>
-            <td>10/18/1985</td>
-            <td>4</td>
-            <td>0.1</td>
-            <td>$50</td>
-            <td>Usa</td>
-            <td>UTA</td>
-          </tr>
-          <tr>
-            <td>Gloria</td>
-            <td>Reeves</td>
-            <td>67439</td>
-            <td>10/18/1985</td>
-            <td>4</td>
-            <td>0.1</td>
-            <td>$50</td>
-            <td>Usa</td>
-            <td>UTA</td>
-          </tr>
-        </tbody>
-      </table>
+          <tbody>
+            {users.length > 0
+              ? users.map((user) => <TableItem {...user} />)
+              : ""}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
--- testWork/src/components/TableItem.jsx
+++ testWork/src/components/TableItem.jsx
@@ -1,9 +1,29 @@
-const TableItem = () => {
-    return ( <>
-    
-    <h1>TableItem</h1>
-    
-    </> );
-}
- 
+const TableItem = ({
+  id,
+  firstName,
+  lastName,
+  maidenName,
+  age,
+  gender,
+  email,
+  phone,
+  address,
+}) => {
+  return (
+    <>
+      <tr key={id}>
+        <td>{firstName}</td>
+        <td>{lastName}</td>
+        <td>{maidenName || "-"}</td>
+        <td>{age}</td>
+        <td>{gender}</td>
+        <td>{phone}</td>
+        <td>{email}</td>
+        <td>{address?.country || "-"}</td>
+        <td>{address?.city || "-"}</td>
+      </tr>
+    </>
+  );
+};
+
 export default TableItem;
