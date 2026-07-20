67d0107b5ac1225c7e48a312b61ba63c2caa9a60
AlexPronin23 <apronin2005@gmail.com>
2026-07-20 15:25:43 +0700

    Added select(optgroup,option)

--- testWork/src/components/Table.jsx
+++ testWork/src/components/Table.jsx
@@ -5,12 +5,22 @@ const Table = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const [currentPage, setCurrentPage] = useState(1); // Текущаю страница
+  const [totalPage, setTotalPage] = useState(0); // Общее количество страниц
+  const limit = 5;
 
-  async function fetchUsers() {
+  async function fetchUsers(page = 1, sortBy = null, order = null) {
+    setLoading(true);
+    setError("");
     try {
-      const response = await fetch(
-        "https://dummyjson.com/users?limit=5&select=firstName,lastName,maidenName,age,gender,email,phone,address",
-      );
+      const skip = (page - 1) * limit;
+      let url = `https://dummyjson.com/users?&limit=${limit}&skip=${skip}&select=firstName,lastName,maidenName,age,gender,email,phone,address`;
+
+      if (sortBy && order) {
+        url += `&sortBy=${sortBy}&order=${order}`;
+      }
+
+      const response = await fetch(url);
 
       if (!response.ok) {
         let message = `Error happened :  ${response.status} - ${response.statusText}`;
@@ -20,6 +30,8 @@ const Table = () => {
       const data = await response.json();
 
       setUsers(data.users);
+      setTotalPage(Math.ceil(data.total / limit));
+      setCurrentPage(page);
       setLoading(false);
     } catch (error) {
       console.log(error.message);
@@ -27,8 +39,20 @@ const Table = () => {
     }
   }
 
+  const goToNextPage = () => {
+    if (currentPage < totalPage) {
+      fetchUsers(currentPage + 1);
+    }
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      fetchUsers(currentPage - 1);
+    }
+  };
+
   useEffect(() => {
-    fetchUsers();
+    fetchUsers(1);
   }, []);
 
   return (
@@ -60,6 +84,51 @@ const Table = () => {
           </tbody>
         </table>
       )}
+
+      <div className="table__control">
+        <p>Сортировать по :</p>
+        <select className="table__select">
+          <optgroup label="Возрастанию">
+            <option value="firstName-asc">Имя</option>
+            <option value="lastName-asc">Фамилия</option>
+            <option value="maidenName-asc">Отчество</option>
+            <option value="age-asc">Возраст</option>
+            <option value="gender-asc">Пол</option>
+            <option value="phone-asc">Номер телефона</option>
+          </optgroup>
+          <optgroup label="Убыванию">
+            <option value="firstName-desc">Имя</option>
+            <option value="lastName-desc">Фамилия</option>
+            <option value="maidenName-desc">Отчество</option>
+            <option value="age-desc">Возраст</option>
+            <option value="gender-desc">Пол</option>
+            <option value="phone-desc">Номер телефона</option>
+          </optgroup>
+          <option value="withoutSort">Без сортировки</option>
+        </select>
+      </div>
+
+      <div className="table__pagination">
+        <button
+          className="table__previous"
+          onClick={goToPreviousPage}
+          disabled={currentPage === 1}
+        >
+          Назад
+        </button>
+
+        <span className="table__total">
+          {currentPage} из {totalPage}
+        </span>
+
+        <button
+          className="table__next"
+          onClick={goToNextPage}
+          disabled={currentPage === totalPage}
+        >
+          Вперед
+        </button>
+      </div>
     </div>
   );
 };
