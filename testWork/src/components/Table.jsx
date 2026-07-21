169b4724454bdbc2671cb4426fdce3edff878480
AlexPronin23 <apronin2005@gmail.com>
2026-07-21 22:38:42 +1000

    Added logic for sort field and  select for filter without logic

--- testWork/src/components/Table.jsx
+++ testWork/src/components/Table.jsx
@@ -7,17 +7,30 @@ const Table = () => {
   const [error, setError] = useState("");
   const [currentPage, setCurrentPage] = useState(1); // Текущаю страница
   const [totalPage, setTotalPage] = useState(0); // Общее количество страниц
-  const limit = 5;
+  const limit = 10; // Количество пользователей на странице
 
-  async function fetchUsers(page = 1, sortBy = null, order = null) {
+  // Функция для взаимодействия с данными пользователей
+  async function fetchUsers(
+    page = 1,
+    sortBy = null,
+    order = null,
+    key = null,
+    value = null,
+  ) {
     setLoading(true);
     setError("");
     try {
       const skip = (page - 1) * limit;
-      let url = `https://dummyjson.com/users?&limit=${limit}&skip=${skip}&select=firstName,lastName,maidenName,age,gender,email,phone,address`;
+      let url = `https://dummyjson.com/users?&limit=${limit}&skip=${skip}&select=firstName,lastName,maidenName,age,gender,email,phone,address,image`;
 
+      // Для работы с сортировкой
       if (sortBy && order) {
         url += `&sortBy=${sortBy}&order=${order}`;
+      }
+
+      // Для работы с фильтрацией
+      if (key && order) {
+        url += `/filter&key=${key}&value=${value}`;
       }
 
       const response = await fetch(url);
@@ -29,9 +42,9 @@ const Table = () => {
 
       const data = await response.json();
 
-      setUsers(data.users);
-      setTotalPage(Math.ceil(data.total / limit));
       setCurrentPage(page);
+      setTotalPage(Math.ceil(data.total / limit));
+      setUsers(data.users);
       setLoading(false);
     } catch (error) {
       console.log(error.message);
@@ -39,17 +52,38 @@ const Table = () => {
     }
   }
 
+  //Функция для перехода на следующию страницу
   const goToNextPage = () => {
     if (currentPage < totalPage) {
       fetchUsers(currentPage + 1);
     }
   };
-
+  // Функция для перехода на предыдущию страницу
   const goToPreviousPage = () => {
     if (currentPage > 1) {
       fetchUsers(currentPage - 1);
     }
   };
+
+  // Функция для сортировки
+  const handleSort = (e) => {
+    const value = e.target.value;
+
+    if (value === "none") {
+      fetchUsers(currentPage);
+      return;
+    }
+
+    const [sortBy, order] = value.split("-");
+
+    if (sortBy && order) {
+    }
+
+    fetchUsers(currentPage, sortBy, order);
+  };
+
+  // // Функция для фильтрации
+  // const handleFilter = (e) => {};
 
   useEffect(() => {
     fetchUsers(1);
@@ -78,28 +112,33 @@ const Table = () => {
           </thead>
 
           <tbody>
-            {users.length > 0
-              ? users.map((user) => <TableItem {...user} />)
-              : ""}
+            {users.length > 0 ? (
+              users.map((user) => <TableItem {...user} />)
+            ) : (
+              <>
+                <span>Пользователей нет</span>
+              </>
+            )}
           </tbody>
         </table>
       )}
 
+      {/* Сортировка */}
       <div className="table__control">
         <p>Сортировать по :</p>
-        <select className="table__select">
+        <select className="table__select" onChange={handleSort}>
           <optgroup label="Возрастанию">
-            <option value="firstName-asc">Имя</option>
-            <option value="lastName-asc">Фамилия</option>
-            <option value="maidenName-asc">Отчество</option>
+            <option value="firstName-asc">Имя(A-Z)</option>
+            <option value="lastName-asc">Фамилия(A-Z)</option>
+            <option value="maidenName-asc">Отчество(A-Z)</option>
             <option value="age-asc">Возраст</option>
             <option value="gender-asc">Пол</option>
             <option value="phone-asc">Номер телефона</option>
           </optgroup>
           <optgroup label="Убыванию">
-            <option value="firstName-desc">Имя</option>
-            <option value="lastName-desc">Фамилия</option>
-            <option value="maidenName-desc">Отчество</option>
+            <option value="firstName-desc">Имя(Z-A)</option>
+            <option value="lastName-desc">Фамилия(Z-A)</option>
+            <option value="maidenName-desc">Отчество(Z-A)</option>
             <option value="age-desc">Возраст</option>
             <option value="gender-desc">Пол</option>
             <option value="phone-desc">Номер телефона</option>
@@ -108,6 +147,18 @@ const Table = () => {
         </select>
       </div>
 
+      {/* Фильтрация */}
+      <div className="table__filter">
+        <p>Фильтровать по: </p>
+        <select className="table__select">
+          <option value="age-more">Возраст(больше 20)</option>
+          <option value="age-less">Возраст(меньше 40)</option>
+          <option value="gender-male">Пол(мужской)</option>
+          <option value="gender-female">Пол(женский)</option>
+        </select>
+      </div>
+
+      {/* Постраничное отображение */}
       <div className="table__pagination">
         <button
           className="table__previous"
