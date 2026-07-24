5b642c8f3af6c80c59fb7052feb0d7a4c40570ea
AlexPronin23 <apronin2005@gmail.com>
2026-07-24 10:19:23 +1000

    Deploy moment

--- testWork/vite.config.js
+++ testWork/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TestWork/', // замените на имя вашего репозитория
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
