50fc74540d448cee8322d735ab4c9bf547f7d4f1
AlexPronin23 <apronin2005@gmail.com>
2026-07-24 10:30:25 +1000

    Deploy was finished

--- testWork/vite.config.js
+++ testWork/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TestWork/', // замените на имя вашего репозитория
+  base: '/TestWork/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
